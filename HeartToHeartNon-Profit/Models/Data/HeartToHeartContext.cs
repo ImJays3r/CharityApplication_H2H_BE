c77cb6da36ea9d080f97607fe391f8b167e73cfb
nguyenthanhliem1708 <nguyenthanhliem1708@gmail.com>
2021-11-05 18:02:13 +0700

    Liem 5-11

--- HeartToHeartNon-Profit/Models/Data/HeartToHeartContext.cs
+++ HeartToHeartNon-Profit/Models/Data/HeartToHeartContext.cs
@@ -22,7 +22,6 @@ namespace HeartToHeartNon_Profit.Models.Data
         public virtual DbSet<CampaignMember> CampaignMembers { get; set; }
         public virtual DbSet<Medium> Media { get; set; }
         public virtual DbSet<Plan> Plans { get; set; }
-        public virtual DbSet<Post> Posts { get; set; }
         public virtual DbSet<Report> Reports { get; set; }
         public virtual DbSet<Task> Tasks { get; set; }
         public virtual DbSet<TaskDetail> TaskDetails { get; set; }
@@ -57,10 +56,7 @@ namespace HeartToHeartNon_Profit.Models.Data
                     .HasMaxLength(200)
                     .IsUnicode(false);
 
-                entity.Property(e => e.Photourl)
-                    .HasMaxLength(10)
-                    .HasColumnName("photourl")
-                    .IsFixedLength(true);
+                entity.Property(e => e.Photourl).IsUnicode(false);
 
                 entity.Property(e => e.StartDate).HasColumnType("datetime");
 
@@ -121,30 +117,15 @@ namespace HeartToHeartNon_Profit.Models.Data
             {
                 entity.HasKey(e => e.MediaId);
 
-                entity.Property(e => e.DateAdded).HasColumnType("datetime");
-
-                entity.Property(e => e.Description)
-                    .IsRequired()
-                    .IsUnicode(false);
-
-                entity.Property(e => e.PublicId)
-                    .IsRequired()
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Type)
-                    .IsRequired()
-                    .HasMaxLength(200)
-                    .IsUnicode(false);
-
                 entity.Property(e => e.Url)
                     .IsRequired()
                     .IsUnicode(false);
 
-                entity.HasOne(d => d.Post)
+                entity.HasOne(d => d.Report)
                     .WithMany(p => p.Media)
-                    .HasForeignKey(d => d.PostId)
+                    .HasForeignKey(d => d.ReportId)
                     .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Media_Posts");
+                    .HasConstraintName("FK_Media_Reports");
             });
 
             modelBuilder.Entity<Plan>(entity =>
@@ -174,34 +155,10 @@ namespace HeartToHeartNon_Profit.Models.Data
                     .HasConstraintName("FK_Plans_Campaigns");
             });
 
-            modelBuilder.Entity<Post>(entity =>
-            {
-                entity.Property(e => e.Content)
-                    .IsRequired()
-                    .HasMaxLength(200)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.CreateDate).HasColumnType("datetime");
-
-                entity.Property(e => e.Description)
-                    .IsRequired()
-                    .HasMaxLength(200)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Tittle)
-                    .IsRequired()
-                    .HasMaxLength(200)
-                    .IsUnicode(false);
-
-                entity.HasOne(d => d.Report)
-                    .WithMany(p => p.Posts)
-                    .HasForeignKey(d => d.ReportId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Posts_Reports");
-            });
-
             modelBuilder.Entity<Report>(entity =>
             {
+                entity.Property(e => e.CreateDate).HasColumnType("date");
+
                 entity.Property(e => e.Description)
                     .IsRequired()
                     .HasMaxLength(200)
--- HeartToHeartNon-Profit/Models/Data/Medium.cs
+++ HeartToHeartNon-Profit/Models/Data/Medium.cs
@@ -8,13 +8,9 @@ namespace HeartToHeartNon_Profit.Models.Data
     public partial class Medium
     {
         public int MediaId { get; set; }
-        public int PostId { get; set; }
-        public string Type { get; set; }
+        public int ReportId { get; set; }
         public string Url { get; set; }
-        public string Description { get; set; }
-        public DateTime DateAdded { get; set; }
-        public string PublicId { get; set; }
 
-        public virtual Post Post { get; set; }
+        public virtual Report Report { get; set; }
     }
 }
